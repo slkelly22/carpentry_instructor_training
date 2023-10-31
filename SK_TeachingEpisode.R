33498c92c1df857f4975f6eece7bc039c1f01cbb
slkelly22 <slkelly@go.olemiss.edu>
2023-10-31 15:24:49 -0500

    Setup the cats data.frame from lesson 4 for the teaching episode for lesson 5

---
+++ SK_TeachingEpisode.R
@@ -0,0 +1,6 @@
+# Teaching Episode
+# Exploring Data Frames in R (Lesson 5)
+
+cats <- data.frame(coat = c("calico", "black", "tabby"), 
+                   weight = c(2.1, 5.0, 3.2), 
+                   likes_string = c(1, 0, 1))
