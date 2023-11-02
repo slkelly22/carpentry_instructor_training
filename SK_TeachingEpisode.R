f1f097c494b4492ac9eb7c8dd2cf1f1aea3b1422
slkelly22 <slkelly@go.olemiss.edu>
2023-11-02 16:36:43 -0500

    results from my live teaching demo

--- SK_TeachingEpisode.R
+++ SK_TeachingEpisode.R
@@ -1,6 +1,18 @@
 # Teaching Episode
 # Exploring Data Frames in R (Lesson 5)
 
+# Make sure you send the data.frame to an object with <-
 cats <- data.frame(coat = c("calico", "black", "tabby"), 
                    weight = c(2.1, 5.0, 3.2), 
                    likes_string = c(1, 0, 1))
+
+cats
+
+age <- c(2, 3, 5)
+
+cbind(cats, age)
+
+age <- c(2, 3)
+
+cbind(cats, age)
+
