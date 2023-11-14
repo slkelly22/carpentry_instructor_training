225b51a0388eac5f2470e5e5d350ad9df9bbf446
slkelly22 <slkelly@go.olemiss.edu>
2023-11-14 15:48:31 -0600

    how far I got in my 5 minute teaching demo today

--- Teaching_Demo.R
+++ Teaching_Demo.R
@@ -3,3 +3,11 @@
 cats <- data.frame(coat = c("calico", "black", "tabby"), 
                    weight = c(2.1, 5.0, 3.2), 
                    likes_string = c(1, 0, 1))
+
+age <- c(2, 3, 5)
+cats
+#add a column
+cbind(cats, age)
+cats
+
+age <- c(2, 3, 5, 12)
